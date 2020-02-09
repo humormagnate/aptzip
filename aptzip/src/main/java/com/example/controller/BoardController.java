a951ec14e709328bcbca879127f27c29433bbf38
이두영 <@doo>
2020-02-09 14:21:38 +0900

    doo boardController수정

--- aptzip/src/main/java/com/example/controller/BoardController.java
+++ aptzip/src/main/java/com/example/controller/BoardController.java
@@ -4,10 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Board;
@@ -38,7 +38,7 @@ public class BoardController {
 		
 	}
 	
-	@GetMapping("/write")
+	@PostMapping("/write")
 	public void writePOST(Model model) {
 	}
 }
