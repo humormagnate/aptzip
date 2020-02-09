434ffeaba669c1f802ef309e106d1e89e460a89b
humormagnate <humormagnate@gmail.com>
2020-02-09 22:14:17 +0900

    로그인 완성

--- aptzip/src/main/java/com/example/config/security/SecurityConfig.java
+++ aptzip/src/main/java/com/example/config/security/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.example.config.security;
 
-import static com.example.config.security.ApplicationUserRole.ADMIN;
-import static com.example.config.security.ApplicationUserRole.USER;
-
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,70 +9,72 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import com.example.config.security.auth.ApplicationUserService;
+import com.example.domain.UserRole;
+import com.example.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   private final PasswordEncoder passwordEncoder;
-  private final ApplicationUserService applicationUserService;
+  private final UserService userService;
 
   @Autowired
-	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
+	public SecurityConfig(PasswordEncoder passwordEncoder, UserService userService) {
   	this.passwordEncoder = passwordEncoder;
-  	this.applicationUserService = applicationUserService;
+  	this.userService = userService;
   }
 
-//	@Override
-//	public void configure(WebSecurity web) throws Exception {
-//		web.ignoring().antMatchers("/node_modules/**", "/js/**", "/img/**", "/lib/**", "/h2-console/**");
-//	}
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/js/**", "/images/**", "/lib/**");
+	}
   
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
       .csrf().disable();
       	// .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-      	// .ignoringAntMatchers("/h2-console/**")
+        // .ignoringAntMatchers("/h2-console/**")
+    
     http
-        .authorizeRequests()
-        .antMatchers("/", "index", "/css/**", "/js/**", "/images/**", "/lib/**", "/h2-console/**").permitAll()
-        .antMatchers("/admin/**").hasRole(ADMIN.name())
-        .anyRequest()
-        .authenticated()
+      .authorizeRequests()
+        .antMatchers("/**").permitAll()
+        .antMatchers("/admin/**").hasRole("ADMIN")
+        .antMatchers("/user/myinfo").hasRole("USER")
+        .anyRequest().authenticated()
       // .and()
       //  .exceptionHandling().accessDeniedPage("/denied")
-      .and()
-      	.headers().frameOptions().disable()
-      .and()
-      	.httpBasic()
-			.and()
-				.formLogin()
+        .and()
+      .headers()
+        .frameOptions().disable()
+        .and()
+      .httpBasic()
+			  .and()
+			.formLogin()
 				.loginPage("/user/go/login")
 	      .loginProcessingUrl("/user/login")
-				.defaultSuccessUrl("/", true)
-				.usernameParameter("userName")
+        .defaultSuccessUrl("/", true)
+        //.failureUrl("/user/go/login")
+				.usernameParameter("email")
 				.passwordParameter("password")
+        .successHandler(successHandler())
 				.permitAll()
-      .and()
-	      .rememberMe()
-	      .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
-	      .key("somethingverysecured")
-	      .rememberMeParameter("remember-me")
-      .and()
-      	.logout()
+        .and()
+	    // .rememberMe()
+	    //   .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+	    //   .key("somethingverysecured")
+	    //   .rememberMeParameter("remember-me")
+      //   .and() => userdetailsservice is required (https://www.boraji.com/spring-security-5-remember-me-authentication-example-with-hibernate-5)
+      .logout()
         .logoutUrl("/user/logout")
         .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout", "GET")) // https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/config/annotation/web/configurers/LogoutConfigurer.html
         .clearAuthentication(true)
@@ -85,30 +84,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
       ;
   }
 
-//	@Override
-//	@Bean
-//	protected UserDetailsService userDetailsService() {
-//		UserDetails user = User.builder().username("user").password(passwordEncoder.encode("user"))
-//				.authorities(USER.getGrantedAuthorities()).build();
-//
-//		UserDetails admin = User.builder().username("admin").password(passwordEncoder.encode("admin"))
-//				.authorities(ADMIN.getGrantedAuthorities()).build();
-//
-//		return new InMemoryUserDetailsManager(user, admin);
-//
-//	}
-
   @Autowired
   protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth.authenticationProvider(daoAuthenticationProvider());
   }
-  
- 
+
   @Bean
   public DaoAuthenticationProvider daoAuthenticationProvider() {
     DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
     provider.setPasswordEncoder(passwordEncoder);
-    provider.setUserDetailsService(applicationUserService);
+    provider.setUserDetailsService(userService);
     return provider;
+  }
+
+  @Bean
+  public AuthenticationSuccessHandler successHandler() {
+    return new LoginSuccessHandler("/");
   }
 }
--- aptzip/src/main/java/com/example/config/security/auth/ApplicationUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.config.security.auth;
-
-import java.util.Optional;
-
-public interface ApplicationUserDao {
-	
-	Optional<ApplicationUser> selectApplicationUserByUsername(String username);
-
-}
